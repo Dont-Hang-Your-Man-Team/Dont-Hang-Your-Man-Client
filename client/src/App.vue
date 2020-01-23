ed45d05631fa6e35ccaa7835cfc3f0f26ea0f6db
vaniairnanda <vaniairnanda@gmail.com>
2020-01-24 00:12:27 +0700

    layout match

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/match">Match</router-link>
     </div>
     <router-view/>
   </div>
@@ -15,6 +15,8 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-image: url("https://cameronscookware.com/wp-content/uploads/2019/12/graph-paper-wallpaper-best-of-1000-images-about-paper-refs-on-pinterest-2019-of-graph-paper-wallpaper.gif");
+
 }
 
 #nav {
---
+++ client/src/views/Match.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <b-button pill variant="warning">hints</b-button>
+    </div>
+    <div id="questions" class="col">
+      _ _ _ _ _ _
+    </div>
+  </div>
+  <br>
+  <br>
+  <div class="row">
+    <div class="col">
+      <img src="https://hangman.doncolton.com/play/hang0.png">
+    </div>
+    <div id="choices" class="col">
+      <b-button id="letterbutton" v-for="(letter, i) in letters" :key="i" variant="outline-primary">{{letter}}</b-button>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      letters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    }
+  }
+}
+</script>
+
+<style scoped>
+#letterbutton {
+  border-radius: 50%;
+}
+#choices {
+  padding-top: 2em;
+}
+#questions {
+  padding-top: 2em;
+}
+</style>
