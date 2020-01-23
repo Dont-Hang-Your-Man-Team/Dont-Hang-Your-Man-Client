29bace41f779685cf49b5483486e45c6c4295b11
gandasipayung <gandasipayung14@gmail.com>
2020-01-23 21:19:46 +0700

    layouting masih pake card, nambahin css bootsrap di index.html

---
+++ client/src/views/EndGame.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <Status class="col-md"/>
+    </div>
+    <UserCard class="row justify-content-center" />
+    <div class="row">
+      <GuessedLetter class="col-md" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Status from '../components/Status'
+import UserCard from '../components/UserCard'
+import GuessedLetter from '../components/GuessedLetter'
+export default {
+  name: 'endgame',
+  components: {
+    Status,
+    UserCard,
+    GuessedLetter
+  }
+}
+</script>
+
+<style>
+
+</style>
